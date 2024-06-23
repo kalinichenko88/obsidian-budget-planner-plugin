e72aa0513f06277983609046ddd61cc07c9ab77e
Ivan Kalinichenko <ivan.kalinichenko@apliteni.com>
2024-06-23 18:40:38 +0400

    improve render a table

--- src/BudgetCodeBlock/models/BlockData.ts
+++ src/BudgetCodeBlock/models/BlockData.ts
@@ -1,3 +1,6 @@
 import type { Row } from './Row';
+import type { Meta } from './Meta';
 
-export type BlockData = Map<string, Row[]>;
+export type BlockDataValue = { rows: Row[]; meta: Meta };
+
+export type BlockData = Map<string, BlockDataValue>;
