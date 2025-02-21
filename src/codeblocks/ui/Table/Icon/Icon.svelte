e251f3439876d2ad20841e73eeffd35cd3fb574a
Ivan Kalinichenko <ivan.kalinichenko@apliteni.com>
2025-02-21 10:16:40 +0400

    add icon component

---
+++ src/codeblocks/ui/Table/Icon/Icon.svelte
@@ -0,0 +1,28 @@
+<div class="icon" bind:this={iconContainer}></div>
+
+<script lang="ts">
+	import { getIcon } from 'obsidian';
+	import { onMount } from 'svelte';
+
+	type Props = {
+		name: string;
+	}
+
+	const { name }: Props = $props();
+
+	let iconContainer: HTMLDivElement;
+
+	onMount(() => {
+		const icon = getIcon(name);
+		if (iconContainer && icon) {
+			iconContainer.appendChild(icon);
+		}
+	});
+</script>
+
+<style>
+	.icon {
+		max-width: var(--icon-size);
+		width: auto;
+	}
+</style>
