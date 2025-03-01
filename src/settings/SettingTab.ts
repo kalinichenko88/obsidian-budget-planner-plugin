8581d835e57d75fcb64341441c32435ab3aa0f53
Ivan Kalinichenko <ivan.kalinichenko@apliteni.com>
2025-03-01 18:35:34 +0400

    refactor: standardize indentation and formatting in Settings, SettingTab, and Plugin files

--- src/settings/SettingTab.ts
+++ src/settings/SettingTab.ts
@@ -3,26 +3,26 @@ import { App, PluginSettingTab, Setting } from 'obsidian';
 import Plugin from '../Plugin';
 
 export class SettingTab extends PluginSettingTab {
-	plugin: Plugin;
+  plugin: Plugin;
 
-	constructor(app: App, plugin: Plugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: Plugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	public display(): void {
-		const { containerEl } = this;
+  public display(): void {
+    const { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		new Setting(containerEl).setName('Default value for budget block').addTextArea((text) =>
-			text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.defaultBudgetBlock)
-				.onChange(async (value) => {
-					this.plugin.settings.defaultBudgetBlock = value;
-					await this.plugin.saveSettings();
-				}),
-		);
-	}
+    new Setting(containerEl).setName('Default value for budget block').addTextArea((text) =>
+      text
+        .setPlaceholder('Enter your secret')
+        .setValue(this.plugin.settings.defaultBudgetBlock)
+        .onChange(async (value) => {
+          this.plugin.settings.defaultBudgetBlock = value;
+          await this.plugin.saveSettings();
+        })
+    );
+  }
 }
