025edc6d2240648ee96dfd71fb772365f0781386
Ivan Kalinichenko <ivan.kalinichenko@apliteni.com>
2025-03-01 11:32:49 +0400

    add Cell component for editable table cells

---
+++ src/codeblocks/ui/componets/Table/Cell/Cell.svelte
@@ -0,0 +1,95 @@
+
+{#if isEditing}
+	<input
+		class="input"
+		bind:this={inputElement}
+		value={value}
+		type={type}
+		min={type === 'number' ? '0' : undefined}
+		step={type === 'number' ? '0.10' : undefined}
+		oninput={handleOnChange}
+		onkeydown={handleOnEnter}
+	/>
+{:else}
+	<div
+		class="text"
+		role="button"
+		tabindex="0"
+		onclick={handleOnClick}
+		onkeydown={handleOnEnter}
+	>
+		{value}
+	</div>
+{/if}
+
+<script lang="ts">
+	import { getContext } from 'svelte';
+
+	import type { StoreActions } from '../actions';
+	import type { TableStore } from '../../../../models';
+	import { debounce } from '../../../helpers/debounce';
+	import { STORE_ACTIONS_CONTEXT_KEY, STORE_CONTEXT_KEY } from '../constants';
+
+	type Props = {
+		value: string;
+		type?: 'text' | 'number';
+		onChange: (value: string) => void;
+	}
+
+	const { value, type = 'text', onChange }: Props = $props();
+
+	const store = getContext<TableStore>(STORE_CONTEXT_KEY);
+	const { selectRow } = getContext<StoreActions>(STORE_ACTIONS_CONTEXT_KEY);
+
+	let isEditing = $state(false);
+	let inputElement: HTMLInputElement | null = $state(null);
+
+	const selectedRowId = $derived($store.selectedRowId);
+
+	$effect(() => {
+		if (isEditing && inputElement) {
+			inputElement.focus();
+		}
+	});
+
+	$effect(() => {
+		if (selectedRowId !== '') {
+			isEditing = false;
+		}
+	});
+
+	export const handleOnChange = debounce((event: Event) => {
+		onChange((event.target as HTMLInputElement).innerText);
+	}, 300);
+
+	export const handleOnClick = (): void => {
+		isEditing = true;
+	};
+
+	export const handleOnEnter = (event: KeyboardEvent): void => {
+		if (event.key === 'Enter') {
+			isEditing = false;
+		}
+	};
+</script>
+
+<style>
+	.input, .input:focus {
+		margin: 0;
+		padding: 0;
+		border: 1px solid red;
+		outline: none;
+		background: transparent;
+		font: inherit;
+		color: inherit;
+		box-shadow: none;
+		width: 100%;
+	}
+
+	.text {
+		display: flex;
+		align-items: center;
+		height: var(--input-height);
+		line-height: normal;
+	}
+</style>
