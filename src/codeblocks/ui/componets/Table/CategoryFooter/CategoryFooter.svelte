7a13385f28bde36c3c06fe92c876d5650446562c
Ivan Kalinichenko <ivan.kalinichenko@apliteni.com>
2025-03-13 11:42:19 +0400

    feat: enhance CategoryFooter and Footer components to display unchecked rows count and sum

--- src/codeblocks/ui/componets/Table/CategoryFooter/CategoryFooter.svelte
+++ src/codeblocks/ui/componets/Table/CategoryFooter/CategoryFooter.svelte
@@ -13,14 +13,16 @@
 
   const store = getContext<TableStore>(STORE_CONTEXT_KEY);
   const rows = $derived($store.rows.get(categoryId) || []);
+  const unselectedRows = $derived(rows.filter((row) => !row.checked));
 
-  let rowsCount: number = $state(0);
-  let rowsSum: number = $state(0);
+  const rowsCount = $derived(rows.length);
+  const rowsSum = $derived(rows.reduce((acc, value) => acc + value.amount, 0));
 
-  $effect(() => {
-    rowsCount = rows.length;
-    rowsSum = rows.reduce((acc, value) => acc + value.amount, 0);
-  });
+  const uncheckedRowsCount = $derived(unselectedRows.length);
+  const uncheckedRowsSum = $derived(unselectedRows.reduce((acc, value) => acc + value.amount, 0));
+
+  const isUnselectedShown = $derived(uncheckedRowsCount > 0 && uncheckedRowsCount !== rowsCount);
+  const isUnselectedSumShown = $derived(uncheckedRowsSum > 0 && uncheckedRowsSum !== rowsSum);
 </script>
 
 <tr class="meta">
@@ -28,10 +30,18 @@
   <td class="cell">
     <span>COUNT:</span>
     {rowsCount}
+    {#if isUnselectedShown}
+      <br />
+      <span>UNCHECKED:</span>{uncheckedRowsCount}
+    {/if}
   </td>
   <td class="cell">
     <span>SUM:</span>
     {moneyFormatter.format(rowsSum)}
+    {#if isUnselectedSumShown}
+      <br />
+      <span>UNCHECKED:</span>{moneyFormatter.format(uncheckedRowsSum)}
+    {/if}
   </td>
   <td></td>
 </tr>
@@ -39,6 +49,7 @@
 <style>
   .cell {
     text-align: end;
+    white-space: nowrap;
     font-size: var(--font-smallest);
   }
 
--- src/codeblocks/ui/componets/Table/Footer/Footer.svelte
+++ src/codeblocks/ui/componets/Table/Footer/Footer.svelte
@@ -1,10 +1,55 @@
 <script lang="ts">
+  import { getContext } from 'svelte';
+
+  import type { TableStore } from '../../../../models';
+  import { moneyFormatter } from '../../../helpers/moneyFormatter';
+  import { STORE_CONTEXT_KEY } from '../constants';
+
+  const store = getContext<TableStore>(STORE_CONTEXT_KEY);
+
+  const rowsCount = $derived.by(() => {
+    return Array.from($store.rows.values()).reduce((acc, rows) => acc + rows.length, 0);
+  });
+  const rowsSum = $derived.by(() => {
+    return Array.from($store.rows.values()).reduce(
+      (acc, rows) => acc + rows.reduce((acc, row) => acc + row.amount, 0),
+      0
+    );
+  });
+
+  const unselectedRowsCount = $derived.by(() => {
+    return Array.from($store.rows.values()).reduce(
+      (acc, rows) => acc + rows.filter((row) => !row.checked).length,
+      0
+    );
+  });
+  const unselectedRowsSum = $derived.by(() => {
+    return Array.from($store.rows.values()).reduce(
+      (acc, rows) =>
+        acc + rows.filter((row) => !row.checked).reduce((acc, row) => acc + row.amount, 0),
+      0
+    );
+  });
+
+  const isUnselectedShown = $derived(unselectedRowsCount > 0 && unselectedRowsCount !== rowsCount);
+  const isUnselectedSumShown = $derived(unselectedRowsSum > 0 && unselectedRowsSum !== rowsSum);
 </script>
 
 <tfoot>
   <tr class="summary">
-    <th colspan="2">Footer</th>
-    <th>100</th>
+    <th colspan="2">
+      <span>COUNT:</span>{rowsCount}
+      {#if isUnselectedShown}
+        <br />
+        <span>UNCHECKED:</span>{unselectedRowsCount}
+      {/if}
+    </th>
+    <th
+      ><span>SUM:</span>{moneyFormatter.format(rowsSum)}
+      {#if isUnselectedSumShown}
+        <br />{moneyFormatter.format(unselectedRowsSum)}
+      {/if}
+    </th>
     <th></th>
   </tr>
 </tfoot>
@@ -12,5 +57,12 @@
 <style>
   .summary > th {
     text-align: end;
+
+    & > span {
+      font-size: var(--font-smallest);
+      color: var(--text-faint);
+      font-weight: var(--font-normal);
+      margin-right: 0.2rem;
+    }
   }
 </style>
