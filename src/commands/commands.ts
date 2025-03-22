43d21f156620fe9d49b57778705223b26eb6547a
Ivan Kalinichenko <ivan.kalinichenko@apliteni.com>
2025-03-22 19:48:15 +0400

    refactor: remove CodeFormatCommand and its registration from commands

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,24 +1,15 @@
 import { Editor, Plugin } from 'obsidian';
 
 import type { Settings } from '../settings';
-import { CodeFormatCommand } from './CodeFormatCommand';
 
 type BudgetPlugin = Plugin & { settings: Settings };
 
-export const registerCommands = (plugin: BudgetPlugin) => {
+export const registerCommands = (plugin: BudgetPlugin): void => {
   plugin.addCommand({
     id: 'insert-budget-planner',
     name: 'Insert Budget Planner',
     editorCallback: (editor: Editor) => {
       editor.replaceSelection('```budget\n' + plugin.settings.defaultBudgetBlock + '\n```');
-    },
-  });
-
-  plugin.addCommand({
-    id: 'format-budget-planner',
-    name: 'Format Code Blocks',
-    editorCallback: (editor: Editor) => {
-      new CodeFormatCommand().handle(editor);
     },
   });
 };
