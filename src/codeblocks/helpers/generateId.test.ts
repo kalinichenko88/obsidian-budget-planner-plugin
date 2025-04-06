f3400125cb199fe289d6c0b298732bb724de4bc1
Ivan Kalinichenko <ivan.kalinichenko@apliteni.com>
2025-04-06 21:12:44 +0400

    test: enhance generateId tests with additional type and format checks

--- src/codeblocks/helpers/generateId.test.ts
+++ src/codeblocks/helpers/generateId.test.ts
@@ -6,11 +6,20 @@ describe('generateId', () => {
   test('should generate a unique ID', () => {
     const id1 = generateId();
     const id2 = generateId();
+
+    expect(id1).toBeTypeOf('string');
+    expect(id2).toBeTypeOf('string');
     expect(id1).not.toEqual(id2);
   });
 
   test('should generate a string of length 21', () => {
     const id = generateId();
+    expect(id).toBeTypeOf('string');
     expect(id.length).toBe(21);
+  });
+
+  test('should generate a valid ID format', () => {
+    const id = generateId();
+    expect(id).toMatch(/^[a-zA-Z0-9_-]{21}$/);
   });
 });
